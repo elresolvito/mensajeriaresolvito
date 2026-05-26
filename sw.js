4fb89dac837ebc92d7589f6f001fefbdae1c69ab
jccarlos010277-cmd <jccarlos010277@gmail.com>
2026-05-26 08:35:47 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'resolvito-v1';
+const urlsToCache = [
+  '/mensajeriaelresolvito/',
+  '/mensajeriaelresolvito/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
